358648101558eeb472cadaf56092df0a7c95c5e2
jamie gist <jamie.gist.7@gmail.com>
2024-11-18 06:00:22 -0800

    updated styling for homepage. Need to fix image alignment.

--- store/src/Home.js
+++ store/src/Home.js
@@ -1,16 +1,20 @@
 import './Home.css';
 import Shop from './Shop';
 import { Link } from "react-router-dom";
-import HeroImage from './images/hero-img.webp';
+import HeroImage from './images/hero-img.jpg';
 
 export default function Home() {
     return (
         <div className='homepage'>
         <h1>Vetêments</h1>
-        <img src={HeroImage} alt='group of women walking'/>
-        <button>
-            <Link className="shop-button" to="shop">Shop Now</Link>
-        </button>
+        <div className='hero-section'>
+            <img src={HeroImage} className='hero-img' alt='portait of three women'/>
+            <div class="middle">
+                <div class="text">
+                    <Link className='shop-link' to='shop'>Shop Now</Link>
+                    </div>
+            </div>
+        </div>
         </div>
     );
 }
